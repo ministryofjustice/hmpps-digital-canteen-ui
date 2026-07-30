7dbf8515b9fe335d77af1d71a570d5e01dc05e30
neeta.shinde <neeta.shinde@justice.gov.uk>
2026-07-30 16:44:15 +0100

    draft changes.

--- integration_tests/pages/pin-phone/homePage.ts
+++ integration_tests/pages/pin-phone/homePage.ts
@@ -8,8 +8,8 @@ export default class HomePage extends AbstractPage {
 
   private constructor(page: Page) {
     super(page)
-    this.header = page.locator('h1')
-    this.usersName = page.getByTestId('header-user-name')
+    this.header = page.locator('h1', { hasText: 'This site is under construction...' })
+    this.usersName = page.getByTestId('launchpad-home-header-user-name')
   }
 
   static async verifyOnPage(page: Page): Promise<HomePage> {
