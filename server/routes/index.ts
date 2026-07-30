b288265404dc0328d05b439a5046c6ed3ce5a969
neeta.shinde <neeta.shinde@justice.gov.uk>
2026-07-30 16:53:48 +0100

    refactore tests

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 
 import type { Services } from '../services'
-import launchpadRoutes from './pin-phone/fake-launchpad'
 import pinPhoneRoutes from './pin-phone/pin-phone-landing'
 import buyPinPhoneCreditRoutes from './pin-phone/buy-pin-phone-credit'
 import checkOrderDetailsRoutes from './pin-phone/check-order-details'
@@ -14,10 +13,9 @@ export default function routes({ auditService }: Services): Router {
   const router = Router()
 
   router.get('/', async (req, res, _next) => {
-    res.redirect('/launchpad')
+    res.redirect('/pin-phone')
   })
 
-  launchpadRoutes(router)
   pinPhoneRoutes(router, auditService)
   buyPinPhoneCreditRoutes(router, auditService)
   checkOrderDetailsRoutes(router, auditService)
