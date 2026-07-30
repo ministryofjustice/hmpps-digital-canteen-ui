11ab65828aa534e8f23e76b4caec4072557714c7
neeta.shinde <neeta.shinde@justice.gov.uk>
2026-07-30 11:37:03 +0100

    added Erlestoke into active agencies list.

--- server/constants/activeAgencies.ts
+++ server/constants/activeAgencies.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const ACTIVE_AGENCIES = ['BWI', 'CKI', 'RNI'] as const
+export const ACTIVE_AGENCIES = ['BWI', 'CKI', 'RNI', 'EEI'] as const
