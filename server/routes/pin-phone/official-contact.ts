3d014d4f8bcf6c66f390461f08da0d2fccfd78cd
drummondjm <mark.drummond@justice.gov.uk>
2026-08-07 15:55:53 +0100

    update intreg tests

--- server/routes/pin-phone/official-contact.ts
+++ server/routes/pin-phone/official-contact.ts
@@ -5,25 +5,11 @@ import config from '../../config'
 export default function viewOfficialContactsRoutes(router: Router, auditService: AuditService): Router {
   router.get('/pin-phone/view-contacts/official-contact/:contactId', async (req, res, _next) => {
     await auditService.logPageView(Page.OFFICIAL_CONTACT, { who: res.locals.user.username, correlationId: req.id })
-
-    const { contactId } = req.params
-    // todo update when API is implemented
-    const contact = {
-      id: contactId,
-      name: 'John Doe',
-      dateAdded: '13 March 2026',
-      type: 'Official',
-      firstName: 'John',
-      lastName: 'Doe',
-      organisation: 'Fake Solicitor inc',
-      relationship: 'Solicitor',
-      telephoneNumber1: '0123456789',
-      telephoneNumber2: '0987654321',
-    }
+    const selectedContact = req.session.allContacts?.find(c => c.id === Number(req.params.contactId))
 
     return res.render('pages/pin-phone/official-contact', {
       pinPhoneApps: config.prisonerAppsUrl,
-      contact,
+      selectedContact,
     })
   })
 
