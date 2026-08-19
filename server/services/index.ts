506038c6dcfb6f45a5d2754128fd8265e91076ff
drummondjm <mark.drummond@justice.gov.uk>
2026-08-19 11:32:49 +0100

    remove example API

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,15 +1,13 @@
 import { dataAccess } from '../data'
 import AuditService from './auditService'
-import ExampleService from './exampleService'
 import PinPhoneService from './pinPhoneService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, exampleApiClient, digitalCanteenApiClient } = dataAccess()
+  const { applicationInfo, hmppsAuditClient, digitalCanteenApiClient } = dataAccess()
 
   return {
     applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
-    exampleService: new ExampleService(exampleApiClient),
     pinPhoneService: new PinPhoneService(digitalCanteenApiClient),
   }
 }
