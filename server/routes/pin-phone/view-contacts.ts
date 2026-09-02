24028a09625adb661ec08ff6c9d6e3d4b68dc2c9
neeta.shinde <neeta.shinde@justice.gov.uk>
2026-09-02 14:38:46 +0100

    reverted local changes

--- server/routes/pin-phone/view-contacts.ts
+++ server/routes/pin-phone/view-contacts.ts
@@ -28,7 +28,7 @@ export default function viewContactsRoutes(
       ],
     ]
 
-    if (allContacts.length > 100) {
+    if (allContacts.length > 0) {
       const sortedContacts = [...allContacts].sort((a, b) => a.name.localeCompare(b.name))
       const totalElements = sortedContacts.length
       const start = currentPage * pageSize
