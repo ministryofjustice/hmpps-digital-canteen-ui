7222dc81ba91dd299fcca44da66a8a396d2aef73
drummondjm <93932689+drummondjm@users.noreply.github.com>
2026-09-21 09:15:49 +0100

    CRC-716 implement pin phone access restriction (#43)
    * implement pin phone access restriction
    
    * implement pin phone access restriction
    
    * Updat to use error constant file
    
    * add WLI to test env access
    
    * add WLI to test env access

--- server/constants/errorMessages.ts
+++ server/constants/errorMessages.ts
@@ -5,6 +5,7 @@ const ERROR_MESSAGE = {
   CREDIT_LIMIT_EXCEEDED_ERROR: 'You cannot have more than £50 phone credit. Enter a smaller amount.',
   POLICY_EVALUATION_ERROR: 'Phone credit could not be added. Try again later.',
   MEDUSA_UNAVAILABLE_MESSAGE: 'Medusa service is currently unavailable',
+  ENTRY_DENIED_ERROR_MESSAGE: 'You do not have access to this service.',
 }
 
 export default ERROR_MESSAGE
